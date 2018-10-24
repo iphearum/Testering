3ba4991ef9c199a4390272caa0f38ff813830318
phearum <38729044+iphearum@users.noreply.github.com>
2018-10-24 07:56:28 +0700

    home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     <h1>{{$title}}</h1>
     <div>
-        <h1>Wellcome to Page Home</h1>
         <p><a href="">click here</a><button class="btn btn-primary">Sign Up</button></p>
     </div>
 @endsection
