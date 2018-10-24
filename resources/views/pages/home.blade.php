1ae3c62b9e12924936d426f872dbadf2da1ec36c
phearum <38729044+iphearum@users.noreply.github.com>
2018-10-24 07:54:47 +0700

    home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,12 +6,4 @@
         <h1>Wellcome to Page Home</h1>
         <p><a href="">click here</a><button class="btn btn-primary">Sign Up</button></p>
     </div>
-    @if(count($services) > 0)
-    <ul class="list-group">
-        @foreach ($services as $service)
-            <li class="list-group-item">{{$service}}</li>
-        @endforeach
-    </ul>
-    @endif
-    @include('inc.services')
 @endsection
